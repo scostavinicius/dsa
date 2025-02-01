2c5cd9b3278c6facb16b42a924b62a4130bda0e4
scostavinicius <2003.costa.vinicius@gmail.com>
2025-02-01 16:37:56 -0300

    fix: Node.hpp

--- include/Node.hpp
+++ include/Node.hpp
@@ -12,13 +12,11 @@ class Node {
  public:
   Node() : proximo(nullptr) {};
   Node(Type dado) : dado(dado), proximo(nullptr) {};
-  Node(Type dado, std::unique_ptr<Node<Type>> proximo)
-      : dado(dado), proximo(proximo) {};
 
   Type getDado() { return dado; };
   void setDado(Type _dado) { dado = _dado; };
 
-  Node<Type> *getProximo() { return proximo.get(); };
+  Node<Type>* getProximo() { return proximo.get(); };
   void setProximo(std::unique_ptr<Node<Type>> prox) {
     proximo = std::move(prox);
   };
